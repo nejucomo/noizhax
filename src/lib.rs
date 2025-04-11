96eedc48e04cf670fe4bc2eb79b21a89606e0165
Nate Wilcox <nejucomo+dev@gmail.com>
2025-04-10 20:07:41 -0700

    Implement serde types and add test parsing of example TOML.

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,41 @@
 //! `noisehax`: (yet another) a library / cli tool for signal processing networks
+use std::collections::BTreeMap;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NodeSpec {
+    description: String,
+    input: Table<Input>,
+    output: Table<Output>,
+    node: Table<WiredNode>,
+}
+
+pub type Table<T> = BTreeMap<String, T>;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Input {
+    r#type: String,
+    description: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Output {
+    r#type: Option<String>,
+    description: Option<String>,
+    from: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct WiredNode {
+    r#type: Option<String>,
+    input: Table<String>,
+}
+
+#[test]
+fn parse_example_note() {
+    dbg!(toml::from_str::<NodeSpec>(include_str!(
+        "../examples/note.toml"
+    )))
+    .unwrap();
+}
